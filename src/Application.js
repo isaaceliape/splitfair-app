08333ee34d1cce7cb3ddca4f31089b9713add193
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 15:29:15 +0100

    Fix image URLs to resolve build warnings

--- src/Application.js
+++ src/Application.js
@@ -49,7 +49,7 @@ class Application extends HTMLElement {
           width: 100vw;
           height: 100vh;
           position: relative;
-            cursor: url(/splitfair-app/cursor-red.png), auto;
+            cursor: url(/cursor-red.png), auto;
           display: flex;
           justify-content: center;
           align-items: center;
