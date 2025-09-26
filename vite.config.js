8bca9fb5e92d9f97ed2f743c811e69d9dd172519
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 12:24:21 +0100

    Add filename option to manifest config

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => ({
         orientation: 'portrait',
         scope: '/',
         start_url: '/',
+        filename: 'manifest.json',
         icons: [
           {
             src: 'icon-72x72.png',
