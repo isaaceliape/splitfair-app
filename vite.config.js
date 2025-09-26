3517d07cf8650aa50c8c67d91fdf10c7cb2913a5
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 12:29:44 +0100

    Change base back to '/splitfair-pwa/' for correct GitHub Pages paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/splitfair-pwa/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
@@ -16,8 +16,8 @@ export default defineConfig(({ mode }) => ({
         background_color: '#3367d6',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/splitfair-pwa/',
+        start_url: '/splitfair-pwa/',
         filename: 'manifest.json',
         icons: [
           {
