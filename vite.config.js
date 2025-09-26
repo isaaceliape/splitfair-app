1462087eade8973f02adc5f5028948f8b3ff0083
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 11:24:57 +0100

    fix: set base to '/' in config, use --base in build command

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/splitfair-pwa/' : '/',
+  base: '/',
   plugins: [
     // Legacy support for older browsers
     legacy({
