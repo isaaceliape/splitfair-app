13813921aedab98915285ef7562e144c714575b1
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 12:34:58 +0100

    Disable navigateFallback to prevent resolution error

--- vite.config.js
+++ vite.config.js
@@ -63,7 +63,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
-        navigateFallback: '/index.html',
+        navigateFallback: false,
         globPatterns: ['**/*.{js,css,ico,png,svg}'],
         maximumFileSizeToCacheInBytes: 5000000, // 5MB
         globIgnores: ['**/desktop_bg.png'], // Exclude large background image
