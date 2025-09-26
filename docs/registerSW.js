37face84caf77b923093f78bb483d9ed4b9cac9c
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 12:20:18 +0100

    Build and commit docs/ for GitHub Pages deployment

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/splitfair-pwa/sw.js', { scope: '/splitfair-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
