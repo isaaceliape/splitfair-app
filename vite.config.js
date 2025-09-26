1cd5f7b95dc13a32b27241b7b8fa8d909cd65ad4
Isaac Eliape <isaaceliape@gmail.com>
2025-09-26 12:33:22 +0100

    Remove html from workbox globPatterns to avoid resolution error

--- vite.config.js
+++ vite.config.js
@@ -63,7 +63,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globPatterns: ['**/*.{js,css,ico,png,svg}'],
         maximumFileSizeToCacheInBytes: 5000000, // 5MB
         globIgnores: ['**/desktop_bg.png'], // Exclude large background image
       },
